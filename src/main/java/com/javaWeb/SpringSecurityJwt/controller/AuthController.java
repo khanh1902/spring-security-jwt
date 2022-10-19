d6a00d5d02e54c7bae107b17d1ac6511fae74c6b
khanh1902 <khanhdt.game@gmail.com>
2022-10-19 22:40:53 +0700

    first commit

--- src/main/java/com/javaWeb/SpringSecurityJwt/controller/AuthController.java
+++ src/main/java/com/javaWeb/SpringSecurityJwt/controller/AuthController.java
@@ -27,7 +27,6 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
